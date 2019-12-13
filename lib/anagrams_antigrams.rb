c34413fc4ba84e6682eaf5d32ae56a690368c5ed
ikemanep1 <ike.esquivelpilloud@gmail.com>
2019-12-13 12:05:30 -0800

    make 2nd test pass

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -6,8 +6,8 @@ class Word
   end
 
   def anagram_check(input1, input2)
-    input1 = @input1.chars.to_a.sort.join()
-    input2 = @input2.chars.to_a.sort.join()
+    input1 = @input1.upcase.chars.to_a.sort.join()
+    input2 = @input2.upcase.chars.to_a.sort.join()
     if (input1 != input2)
       @output = [@input1, " and ", @input2, " are not anagrams"]
       @output.join("")
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -6,4 +6,8 @@ describe('#anagrams_antigrams') do
     test = Word.new("fate", "feat")
     expect(test.anagram_check("fate", "feat")).to(eq("fate and feat are anagrams"))
   end
+  it('checks if functionality accounts for capital letters') do
+    test = Word.new("Fate", "feat")
+    expect(test.anagram_check("Fate", "feat")).to(eq("Fate and feat are anagrams"))
+  end
 end
