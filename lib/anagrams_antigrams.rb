330f85fdb6ad61a377b3c5683ea8d190d5a74ad1
ikemanep1 <ike.esquivelpilloud@gmail.com>
2019-12-13 12:33:55 -0800

    make third test work

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -8,8 +8,15 @@ class Word
   def anagram_check(input1, input2)
     input1 = @input1.upcase.chars.to_a.sort.join()
     input2 = @input2.upcase.chars.to_a.sort.join()
+    word_check = input1.scan(/[AEIOU]/).length()
     if (input1 != input2)
       @output = [@input1, " and ", @input2, " are not anagrams"]
+      @output.join("")
+    elsif (input1 != input2) && (word_check == 0)
+      @output = [@input1, " and ", @input2, " are not anagrams, because they are not words!"]
+      @output.join("")
+    elsif (input1 == input2) && (word_check == 0)
+      @output = [@input1, " and ", @input2, " are not anagrams, because they are not words!"]
       @output.join("")
     else
       @output = [@input1, " and ", @input2, " are anagrams"]
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -10,4 +10,8 @@ describe('#anagrams_antigrams') do
     test = Word.new("Fate", "feat")
     expect(test.anagram_check("Fate", "feat")).to(eq("Fate and feat are anagrams"))
   end
+  it('checks if functionality accounts for capital letters') do
+    test = Word.new("NRSST", "NTSR")
+    expect(test.anagram_check("NRSST", "NTSR")).to(eq("NRSST and NTSR are not anagrams"))
+  end
 end
